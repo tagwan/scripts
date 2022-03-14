a9cb76b488601e529107fab97cd4200ec68c7e52
Espresso <monokai@qq.com>
2022-03-14 11:08:05 +0800

    feat(shell): 部分shell

---
+++ shell/s002_mail.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 发送告警脚本
+
+# yum install mailx
+# vi /etc/mail.rc
+set from=baojingtongzhi@163.com smtp=smtp.163.com
+set smtp-auth-user=baojingtongzhi@163.com smtp-auth-password=123456
+set smtp-auth=login
---
+++ shell/s005_nginx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Nginx 访问访问日志按天切割
+
+LOG_DIR=/usr/local/nginx/logs
+YESTERDAY_TIME=$(date -d "yesterday" +%F)
+LOG_MONTH_DIR=$LOG_DIR/$(date +"%Y-%m")
+LOG_FILE_LIST="default.access.log"for LOG_FILE in $LOG_FILE_LIST; do
+   [ ! -d $LOG_MONTH_DIR ] && mkdir -p $LOG_MONTH_DIR
+   mv $LOG_DIR/$LOG_FILE $LOG_MONTH_DIR/${LOG_FILE}_${YESTERDAY_TIME}
+  done
+
+kill -USR1 $(cat /var/run/nginx.pid)
+
